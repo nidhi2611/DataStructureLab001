75d3c39e6dd5955b4785837aee56cde9e4b85590
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-01-08 16:03:16 +0530

    function for sum and average

--- src/ArrayProcessing/CreateArray.java
+++ src/ArrayProcessing/CreateArray.java
@@ -9,12 +9,21 @@ public class CreateArray {
         int[] array = new int[sizeOfArray];
         CreateArray createArray = new CreateArray();
         createArray.enterElementsInArray(array, sizeOfArray, sc);
-
+        createArray.calculateSumAndAverage(array);
     }
 
     public void enterElementsInArray(int arr[], int k, Scanner scanner) {
         for (int i = 0; i < k; i++) {
             arr[i] = scanner.nextInt();
         }
+    }
+
+    public void calculateSumAndAverage(int arr[]) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("sum = " + sum);
+        System.out.println("Average =" + (sum / arr.length));
     }
 }
